23052c7de7bc5ef6986036ac5f52a1b795ca003a
Michael Dorst <mikefdorst@gmail.com>
2013-12-12 11:40:54 -0800

    Configure RSpec to run in color

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,6 @@
 require 'rspec'
 
-##
-# to enable color, the following code must be present, and
-# one must also use the `-c` flag when using the `rspec` command
 RSpec.configure do |config|
   config.tty = true
+  config.color_enabled = true
 end
